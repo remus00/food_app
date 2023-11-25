9390fcd899cd490b6e31ae9bb34af19557092a3f
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-25 09:29:03 +0100

    Header Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import Header from '@/components/layout/Header';
 
 export default function Home() {
-    return <div>test</div>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
